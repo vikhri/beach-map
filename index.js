038e3c922784b783337d420693f89e95013118b9
ira <vikhrova.irina@gmail.com>
2023-02-09 21:45:04 +0700

    wip

--- index.js
+++ index.js
@@ -1,63 +1,112 @@
+// import '/points.js';
+
+let checkbox = document.getElementById('all');
+console.log(checkbox);
+
 let map;
+
 
 const bophutBeach = { lat: 9.56198785015178, lng: 100.02605438232423 };
 const maenamBeach = { lat: 9.57282136669429, lng: 99.99532699584962 };
 const laemBeach = { lat: 9.568081745691513, lng: 99.91790771484376 };
-const chawengBech = { lat: 9.52118979624974, lng: 100.05884170532228 };
+const chawengBeach = { lat: 9.52118979624974, lng: 100.05884170532228 };
 
-
-
+const beachInfo =
+'<div id="content">' +
+'<h1 id="firstHeading" class="firstHeading">Beach</h1>' +
+'<p>Description</p>' +
+'</div>';
 
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+ 
+    map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 9.50139445, lng: 99.99561927320003 },
     zoom: 12,
   });
 
-  const svgMarker = {
-    path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "blue",
-    fillOpacity: 0.6,
-    strokeWeight: 0,
-    rotation: 0,
-    scale: 2,
-    anchor: new google.maps.Point(0, 20),
-  };
+//   var Layer = new google.maps.Data();
+//   Layer.setMap(map);
+
 
   const image = {
-    url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    url: "/sun-umbrella.png",
     // This marker is 20 pixels wide by 32 pixels high.
-    size: new google.maps.Size(20, 32),
+    size: new google.maps.Size(24, 24),
     // The origin for this image is (0, 0).
     origin: new google.maps.Point(0, 0),
     // The anchor for this image is the base of the flagpole at (0, 32).
-    anchor: new google.maps.Point(0, 32),
+    anchor: new google.maps.Point(0, 24),
   };
 
-  //Придумать как добавить маркеры циклом
+
+  const infowindow = new google.maps.InfoWindow({
+    content: beachInfo,
+  });
+
+    
 
   const marker1 = new google.maps.Marker({
     position: bophutBeach,
-    map: map,
+    // map: map,
+    icon: image,
+    title: "Bophut",
+  });
+
+ 
+  marker1.addListener("click", () => {
+    infowindow.open({
+      anchor: marker1,
+      map,
+    });
+  });
+
+  
+  const marker2 = new google.maps.Marker({
+    position: maenamBeach,
+    // map: map,
     icon: image,
   });
 
-  const marker2 = new google.maps.Marker({
-    position: maenamBeach,
-    map: map,
+  marker2.addListener("click", () => {
+    infowindow.open({
+      anchor: marker2,
+    //   map,
+    });
   });
 
   const marker3 = new google.maps.Marker({
     position: laemBeach,
-    map: map,
+    // map: map,
+    icon: image,
   });
 
-  const lmarker4 = new google.maps.Marker({
-    position: chawengBech,
-    map: map,
+  const marker4 = new google.maps.Marker({
+    position: chawengBeach,
+    // map: map,
+    icon: image,
   });
 
-}
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("Checkbox is checked..");
+      marker1.setMap(map);
+      marker2.setMap(map);
+      marker3.setMap(map);
+      marker4.setMap(map);
+    } else {
+      console.log("Checkbox is not checked..");
+      marker1.setMap(null);
+      marker2.setMap(null);
+      marker3.setMap(null);
+      marker4.setMap(null);
+    }
+  });
+
+
+};
+
+
 
 window.initMap = initMap;
+
