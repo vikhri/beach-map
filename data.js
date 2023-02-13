4739cb50a1934eedcd573283d02e12d020546403
ira <vikhrova.irina@gmail.com>
2023-02-13 14:36:39 +0700

    moduling

---
+++ data.js
@@ -0,0 +1,68 @@
+export let points = [
+  {
+    id: 1,
+    position: { lat: 9.56198785015178, lng: 100.02605438232423 },
+    title: 'bophutBeach',
+    description: '<p>bophutBeach</p>',
+    waves: true,
+    sunset: false,
+  },
+  {
+    id: 2,
+    position: { lat: 9.57282136669429, lng: 99.99532699584962 },
+    title: 'maenamBeach',
+    description: '<p>maenamBeach</p>',
+    waves: true,
+    sunset: false,
+  },
+  {
+    id: 3,
+    position: { lat: 9.568081745691513, lng: 99.91790771484376 },
+    title: 'laemBeach',
+    description: '<p>laemBeach</p>',
+    waves: false,
+    sunset: true,
+  },
+  {
+    id: 4,
+    position: { lat: 9.52118979624974, lng: 100.05884170532228 },
+    title: 'chawengBeach',
+    description: '<p>chawengBeach</p>',
+    waves: true,
+    sunset: false,
+  },
+  {
+    id: 5,
+    position: { lat: 9.556448527658542, lng: 99.9284319755981 },
+    title: 'Nathonbeach',
+    description: '<p>Nathon beach</p>',
+    waves: false,
+    sunset: true,
+  },
+  {
+    id: 6,
+    position: { lat: 9.483801035442333, lng: 99.92600932601103 },
+    title: 'LipaNoibeach',
+    description: '<p>Lipa Noi beach</p>',
+    waves: true,
+    sunset: true,
+  },
+  {
+    id: 7,
+    position: { lat: 9.4641728042101, lng: 100.04622697611381 },
+    title: 'Lamaibeach',
+    description: '<p>Lamai beach</p>',
+    waves: true,
+    sunset: false,
+  },
+  {
+    id: 8,
+    position: { lat: 9.427709368300851, lng: 99.93662395292347 },
+    title: 'PangKhabeach',
+    description: '<p>Pang Kha beach</p>',
+    waves: false,
+    sunset: true,
+  },
+
+];
+
